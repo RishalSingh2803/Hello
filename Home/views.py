b6703994ae7e9469c61a85bd6ecf2e708a1a89ba
Rishal Singh <singhrishal2803@gmail.com>
2023-12-12 04:17:54 +0530

    Data base added

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,HttpResponse
-
+#from datetime import datetime
+from Home.models import Contact
+#from datetime import timezone
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -11,5 +13,12 @@ def services(request):
     return render(request, 'services.html')
     #return HttpResponse("This is Services Page")
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        #desc = request.POST.get('desc')
+        contact = Contact(name = name, email = email, phone = phone)
+        contact.save()
     return render(request, 'contact.html')
     #return HttpResponse("This is Contact Page")
